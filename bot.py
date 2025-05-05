8875f17fd05bf882f127000d4864f30c4931f512
sonyazaeva <sofiya.zaeva@gmail.com>
2025-05-05 13:40:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram import F
 from aiogram import Bot, Dispatcher, types
