2324b4de1e8a1e3cb00aa02d72649554fc440653
MariaSamus <samusmsq@gmail.com>
2025-05-11 01:42:53 +0300

    Update bot.py
    исправление кода с прошлого коммита без ключевых изменений.

--- main/bot.py
+++ main/bot.py
@@ -388,7 +388,3 @@ async def main() -> None:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
